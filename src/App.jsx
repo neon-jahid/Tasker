d83e730b8c8c58c6a36e5272d0e584c78838d7f9
Jahid Hasan <jahid.softwindtech@gmail.com>
2025-05-25 17:39:30 +0600

    nothing to commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <HeroSection />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
